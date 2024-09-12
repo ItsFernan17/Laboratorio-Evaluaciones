f8085c8c8edcc0167086db014eb52b9b5ceb0bda
Fernando Rivas <frivasm@miumg.edu.gt>
2024-09-12 17:01:29 -0600

    Growl y Mensajes de Anulacion

--- Aplicación/Laboratorio de Evaluaciones MINDEF/web/js/script.js
+++
@@ -1,9 +0,0 @@
-function mostrarBotones() {
-    var modificar = document.querySelector(".contenedor .botonMenuModificar");
-    var anular = document.querySelector(".contenedor .botonMenuAnular");  
-    
-    if(modificar){
-        modificar.style.display="inline";
-        anular.style.display="inline";        
-    }
-}
